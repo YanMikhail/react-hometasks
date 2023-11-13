8905d1041cfc239246686f23216a13000e1a9f3e
Mikhail Yan-Chin-San <mikhail.yan-chin-san@axenix.pro>
2023-11-13 14:42:01 +0300

    HW1

--- src/pages/HomeWork1/HomeWork1.tsx
+++ src/pages/HomeWork1/HomeWork1.tsx
@@ -1,3 +1,20 @@
+import {useState} from "react";
+
+function randomInteger(min: number, max: number): number {
+  let rand = min - 0.5 + Math.random() * (max - min + 1)
+  return  Math.round(rand);
+}
+
 export const HomeWork1 = () => {
-  return <div>Решение домашнего задания №1</div>;
+  const [roll, setRoll] = useState<number>();
+  const diceRoll = () => setRoll(randomInteger(1,20));
+
+  return (
+    <div style={{height: 'calc(100vh - 96px)', padding: '15px 0', display: "flex", flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
+      <button onClick={diceRoll} style={{background: '#fff', padding: '10px', border: 'medium dashed blue', borderRadius: '10px'}}>Бросить кубик d20</button>
+      <div style={{marginTop: '100px', fontSize: '24px'}}>
+        Результат броска: <span style={{fontWeight: 'bold', fontSize: '36px'}}>{roll}</span>
+      </div>
+    </div>
+  );
 };
